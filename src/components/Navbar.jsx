2d66e52ff5b2b6d968b2e4e523a81ccaf87f9fc2
Sauzan Faza <sauzanfaza@gmail.com>
2025-08-21 10:49:32 +0700

    create navbar

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,40 @@
+// import { Menu, MenuButton, MenuItem, MenuItems } from "@headlessui/react";
+import { IoLocationSharp } from "react-icons/io5";
+import { IoMdSearch } from "react-icons/io";
+
+export default function Navbar() {
+return (
+    <>
+    <header className="w-full fixed top-0 z-50 p-4 px-20 flex items-center justify-between bg-[#04356A] text-white">
+        <div className="flex items-center pl-4 font-bold text-lg">
+        TRAVEL AGENCY
+        </div>
+
+        <nav className="grid gap-2">
+            <div className="flex items-center text-sm gap-2">
+                <p className="text-orange-500"><IoLocationSharp /></p>
+                <p>FIND US</p>
+                <input type="text" placeholder="search" className="border border-white rounded-md focus:outline-none p-1"/>
+                <button className="text-lg"><IoMdSearch /></button>
+            </div>
+            <div className="flex items-center justify-center gap-2">
+                <p className="text-sm">CALL US TOLL FREE!</p>
+                <p className="text-orange-500 tetx-sm">0123456789</p>
+            </div>
+        </nav>        
+    </header>
+    <nav className="w-full bg-white mt-26">
+    <div className="flex items-center justify-center text-black gap-6 text-sm font-semibold">
+        <a href="">Home</a>
+        <a href="">Tour</a>
+        <a href="">About Us</a>
+        <a href="">Dicover</a>
+        <a href="">Events</a>
+        <a href="">Rewards</a>
+        <a href="">Travel Blog</a>
+        <a href="">Contact Us</a>
+    </div>
+</nav>
+</>
+);
+}
